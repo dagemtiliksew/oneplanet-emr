e9e08213dae6abc30a6c03a720c8dcf6bc024ac7
Kidus Tiliksew <kidus@tensorsystems.net>
2022-05-05 16:54:45 +0300

    Login and registration

---
+++ resources/views/login.blade.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Laravel</title>
+
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+
+
+    <!-- Styles -->
+   
+    <link href="{{ asset('css/login.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/common.css') }}" rel="stylesheet">
+
+  </head>
+  <body>
+   <div class="login">
+      <div class="body">
+        <form>
+          <div style="font-weight: bold; color: rgba(20, 184, 166, 1); font-size: 20px;">
+            Electronic Medical Record
+          </div>
+          <div class="flex" style="justify-content: center; margin-left: 4rem; margin-top: 2.5rem">
+            <div class="flex-initial" style="padding-left: 1.75rem; padding-right: 1.75rem">
+              <p style="font-size: 1.875rem; line-height: 2.25rem; color: rgba(31, 41, 55, 1); font-weight: 700; letter-spacing: 0.025em;">
+                Welcome Back
+              </p>
+
+              <p class="text-teal-500 font-semibold" style="color: rgba(20, 184, 166, 1); font-weight: 600">
+                One Planet
+              </p>
+
+              <div style="margin-top: 4rem; width: 100%;">
+                <input
+                  class="email-input"
+                  type="text"
+                  placeholder="Email"
+                  name="email"
+                  id="email"
+                  required
+              />
+              
+              <input
+                class="password-input"
+                type="password"
+                placeholder="Password"
+                name="password"
+                id="password"
+                required
+              />
+              </div>
+
+              <div class="flex" style="margin-top: 2.5rem;">
+                <div class="flex-1">
+                  <a
+                    class="login-button"
+                    type="submit"
+                    href="/"
+                  >
+                  <p>Login now</p>
+                </a>
+                </div>
+                <div class="flex-1" style="margin-left: 2rem; margin-top: 8px; align-items: center">
+                  <a
+                    class="register-button"
+                    type="button"
+                    href="/register"
+                  >
+                    Create account
+                </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </form>
+      </div>
+   </div>
+  </body>
+</html>
